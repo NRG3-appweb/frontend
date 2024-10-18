be6e70d886f341d278df5e71b44096220f417d5d
NeoRise456 <neorise456@gmail.com>
2024-10-17 21:07:49 -0500

    refactor(client-service-detail): corrected implementation of get service by id.

--- src/service-management/pages/client-service-detail.component.vue
+++ src/service-management/pages/client-service-detail.component.vue
@@ -1,47 +1,73 @@
 <script>
-
 import {ServiceApiService} from "../services/service-api.service.js";
 import {Service} from "../model/service.entity.js";
-
 
 export default {
   name: "client-service-detail",
   components: {},
   data() {
     return {
-      service: Service
+      service: Service,
+      currentService: Service,
+      serviceApiService: new ServiceApiService(),
     };
   },
+  methods: {
+    buildServiceFromResponseData(service) {
+      return new Service(
+          service.id,
+          service.category_id,
+          service.company_id,
+          service.service_name,
+          service.description,
+          service.price,
+          service.duration,
+          service.rating,
+          service.sales,
+          service.created_at,
+          service.img
+      )
+    },
+    getServiceId() {
+      return this.$route.params.id;
+    },
+    getServiceById(id){
+      this.serviceApiService.getServiceById(id)
+          .then(response => {
+            this.currentService = this.buildServiceFromResponseData(response.data);
+          })
+          .catch(error => {
+            console.error("There was an error fetching the service:", error);
+          });
+    },
+    getCurrentService(){
+      let serviceId = this.getServiceId();
+      if (serviceId) {
+        this.getServiceById(serviceId);
+      }
+    }
+  },
   created() {
-    const serviceId = this.$route.params.id;
-    const serviceApi = new ServiceApiService();
-    serviceApi.getService(serviceId)
-        .then(response => {
-          this.service = response.data;
-          console.log(this.service);
-        })
-        .catch(error => {
-          console.error("There was an error fetching the service:", error);
-        });
+    this.getCurrentService();
   }
 };
 </script>
 
 <template>
   <div style="margin-top: 10rem; background-color: #1a1a1a; padding: 5rem; border-radius: 1rem;">
-    <div style="display: flex; gap: 1rem">
+    <div class="flex gap-1">
       <div>
-        <img :src="service.img" alt="Service Image" />
+        <img :src="currentService.img" alt="Service Image" />
       </div>
       <div>
-        <h1>{{ service.service_name }}</h1>
-        <p>{{ $t('clientServiceDetail.description') }}: {{ service.description }}</p>
-        <p>{{ $t('clientServiceDetail.price') }}: {{ service.price }}</p>
-        <p>{{ $t('clientServiceDetail.duration') }}: {{ service.duration }}</p>
+        <h1>{{ currentService.service_name }}</h1>
+        <p>{{ $t('clientServiceDetail.description') }}: {{ currentService.description }}</p>
+        <p>{{ $t('clientServiceDetail.price') }}: {{ currentService.price }}</p>
+        <p>{{ $t('clientServiceDetail.duration') }}: {{ currentService.duration }}</p>
       </div>
     </div>
     <div class="mt-7">
-      <pv-button :label="$t('clientServiceDetail.bookNow')" class="w-full" />
+      <pv-button :label=" $t('clientServiceDetail.bookNow') " class="w-full" />
     </div>
   </div>
 </template>
